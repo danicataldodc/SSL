cd1d245e648ff463230012b06feeb7f546abcce9
Daniela Leonor Cataldo <dcataldo@frba.utn.edu.ar>
2024-04-22 15:02:28 -0300

    Add files via upload

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    printf ("%ld\n", __STDC_VERSION__); //Con este código imprimo por pantalla el compilador que estoy usando.
+
+    printf ("Hola Mundo!\n"); //Con este código imprimo por pantalla un mensaje.
+
+    return 0;
+}
